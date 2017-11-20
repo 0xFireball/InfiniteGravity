ce67024e0d9a23953b733a1201518cdae4e5bd62
0xFireball <0xFireball@outlook.com>
2017-11-20 09:39:55 -0800

    tilemep

---
+++ src/InfiniteGravity/InfiniteGravity/Content/Maps/tilesets/concrete.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="concrete" tilewidth="16" tileheight="16" tilecount="24" columns="2">
+ <image source="../images/concrete.png" width="32" height="192"/>
+</tileset>
