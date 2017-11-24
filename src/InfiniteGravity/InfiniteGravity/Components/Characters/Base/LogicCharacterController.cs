a8eb7ad91d3a23155f05d74f1c8ccd47de35d32f
0xFireball <0xFireball@outlook.com>
2017-11-24 11:15:36 -0800

    wire logical inputs

--- src/InfiniteGravity/InfiniteGravity/Components/Characters/Base/LogicCharacterController.cs
+++ src/InfiniteGravity/InfiniteGravity/Components/Characters/Base/LogicCharacterController.cs
@@ -5,13 +5,19 @@ namespace InfiniteGravity.Components.Characters.Base {
         public VirtualJoystick.LogicJoystick moveDirectionLogical { get; } = new VirtualJoystick.LogicJoystick();
         public VirtualJoystick.LogicJoystick targetDirectionLogical { get; } = new VirtualJoystick.LogicJoystick();
 
+        public VirtualAxis.LogicAxis thrustLogical { get; } = new VirtualAxis.LogicAxis();
+
         public VirtualButton.LogicButton primaryActionLogical { get; } = new VirtualButton.LogicButton();
+        public VirtualButton.LogicButton secondaryActionLogical { get; } = new VirtualButton.LogicButton();
 
         public override void initialize() {
             moveDirectionInput.nodes.Add(moveDirectionLogical);
             targetDirectionInput.nodes.Add(targetDirectionLogical);
 
+            thrustInput.nodes.Add(thrustLogical);
+
             primaryActionInput.nodes.Add(primaryActionLogical);
+            secondaryActionInput.nodes.Add(secondaryActionLogical);
 
             base.initialize();
         }
