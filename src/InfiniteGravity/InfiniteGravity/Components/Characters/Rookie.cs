501b2ec99a83c97930ab88a47e3216112a5e16d0
0xFireball <0xFireball@outlook.com>
2017-11-24 16:49:47 -0800

    add Rookie animation frames

--- src/InfiniteGravity/InfiniteGravity/Components/Characters/Rookie.cs
+++ src/InfiniteGravity/InfiniteGravity/Components/Characters/Rookie.cs
@@ -26,12 +26,40 @@ namespace InfiniteGravity.Components.Characters {
                 _subtextures[4],
                 _subtextures[5]
             }) {fps = 10f});
-            
+
+            Animation.addAnimation(Animations.Melee1, new SpriteAnimation(new List<Subtexture> {
+                _subtextures[6],
+                _subtextures[7],
+                _subtextures[8],
+                _subtextures[9]
+            }) {fps = 10f});
+
+            Animation.addAnimation(Animations.Melee2, new SpriteAnimation(new List<Subtexture> {
+                _subtextures[10],
+                _subtextures[11],
+                _subtextures[12],
+                _subtextures[13]
+            }) {fps = 10f});
+
+            Animation.addAnimation(Animations.Melee3, new SpriteAnimation(new List<Subtexture> {
+                _subtextures[14],
+                _subtextures[15],
+                _subtextures[16],
+                _subtextures[17]
+            }) {fps = 10f});
+
+            Animation.addAnimation(Animations.Gun1, new SpriteAnimation(new List<Subtexture> {
+                _subtextures[18],
+                _subtextures[19],
+                _subtextures[20],
+                _subtextures[21]
+            }) {fps = 10f});
+
             // flip X based on facing direction
             setFacingFlip(true, false);
 
             facing = Direction.Right;
-            
+
             // TODO: Collision
             entity.addComponent(new BoxCollider(-4, -8, 10, 24));
         }
